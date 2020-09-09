a053f2e38d43c9c82a81eadc055e84cee768c3d1
Stepan Reuk <stepan.reuk@3divi.com>
2020-09-09 12:51:51 +0500

    added unsubscribing from event in FaceAnimManager

--- Assets/NuitrackSDK/Tutorials/Animated Emoji/Final Assets/Scripts/FaceAnimManager.cs
+++ Assets/NuitrackSDK/Tutorials/Animated Emoji/Final Assets/Scripts/FaceAnimManager.cs
@@ -56,4 +56,9 @@ public class FaceAnimManager : MonoBehaviour
             }
         }
     }
+
+    private void OnDestroy()
+    {
+        NuitrackManager.onSkeletonTrackerUpdate -= OnSkeletonUpdate;
+    }
 }
