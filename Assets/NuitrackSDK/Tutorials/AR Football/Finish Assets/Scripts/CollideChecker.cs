7551f6db6e62c9fc14f5a39036a4f7df46c6589f
Stepan Reuk <stepan.reuk@3divi.com>
2020-10-14 16:53:13 +0500

    update ar tutorial

---
+++ Assets/NuitrackSDK/Tutorials/AR Football/Finish Assets/Scripts/CollideChecker.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CollideChecker : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponentInParent<BallController>().OnCollide(collision); // If the ball collides with something, then we report this to the BallController, which is located on the parent object
+    }
+}
