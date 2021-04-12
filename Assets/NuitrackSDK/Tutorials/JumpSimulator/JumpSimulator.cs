308db0ad33515367cc8975670b285a706c5d8916
Evgeny <e.vasilev@3divi.com>
2021-04-12 15:01:10 +0500

    Add UI jump simulator draft

---
+++ Assets/NuitrackSDK/Tutorials/JumpSimulator/JumpSimulator.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections.Generic;
+
+public class JumpSimulator : MonoBehaviour
+{
+    [SerializeField] float deltaAnkleFloor = 0.1f;
+    Plane floorPlane;
+
+    [SerializeField] RectTransform baseRect;
+
+    [Header ("Best jump UI")] 
+    [SerializeField] RectTransform bestJumpLine;
+    [SerializeField] Text bestJumpLable;
+
+    [Header("Current jump UI")]
+    [SerializeField] RectTransform currentJumpLine;
+    [SerializeField] Text currentJumpLable;
+
+    [SerializeField] SkeletonEmulator skeletonEmulator;
+
+    public Transform floorBasePointTransform;
+    public Transform floorNormalVectorTransform;
+
+    public float TotalMaxJumpHeigth
+    {
+        get;
+        private set;
+    }
+
+    public float CurrentMaxJumpHeigth
+    {
+        get;
+        private set;
+    }
+
+
+    List<nuitrack.JointType> leftLegJoints = new List<nuitrack.JointType>
+    {
+        nuitrack.JointType.LeftHip,
+        nuitrack.JointType.LeftKnee,
+        nuitrack.JointType.LeftAnkle
+    };
+
+    List<nuitrack.JointType> rightLegJoints = new List<nuitrack.JointType>
+    {
+        nuitrack.JointType.RightHip,
+        nuitrack.JointType.RightKnee,
+        nuitrack.JointType.RightAnkle
+    };
+
+    Vector3 JointPoisition(nuitrack.Skeleton skeleton, nuitrack.JointType jointType)
+    {
+        //return skeleton.GetJoint(jointType).ToVector3() * 0.001f;
+
+        return skeletonEmulator.JointPoisition(jointType);
+    }
+
+    bool LegOnFloor(nuitrack.Skeleton skeleton)
+    {
+        Vector3 leftAnkle = JointPoisition(skeleton, nuitrack.JointType.LeftAnkle);
+        Vector3 rightAnkle = JointPoisition(skeleton, nuitrack.JointType.RightAnkle);
+
+        Vector3 floorLeftAnklePoint = floorPlane.ClosestPointOnPlane(leftAnkle);
+        Vector3 floorRightAnklePoint = floorPlane.ClosestPointOnPlane(rightAnkle);
+
+        return Vector3.Distance(leftAnkle, floorLeftAnklePoint) <= deltaAnkleFloor || Vector3.Distance(rightAnkle, floorRightAnklePoint) <= deltaAnkleFloor;
+    }
+
+    float LegLength(nuitrack.Skeleton skeleton, List<nuitrack.JointType> jointTypes)
+    {
+        float length = 0;
+
+        for (int i = 0; i < jointTypes.Count - 1; i++)
+            length += Vector3.Distance(JointPoisition(skeleton, jointTypes[i]), JointPoisition(skeleton, jointTypes[i + 1]));
+
+        return length;
+    }
+
+    float ProjectHipY(nuitrack.Skeleton skeleton, float delta=0)
+    {
+        //return (skeleton.GetJoint(nuitrack.JointType.LeftHip).Proj.Y + skeleton.GetJoint(nuitrack.JointType.RightHip).Proj.Y) / 2 + delta * 1000;
+
+        Vector3 hipPosition = JointPoisition(skeleton, nuitrack.JointType.LeftHip);
+        hipPosition.y += delta;
+
+        Vector3 hip = Camera.main.WorldToScreenPoint(hipPosition);
+        return hip.y / Screen.height;
+    }
+
+    void Start()
+    {
+        bestJumpLine.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nuitrack.Skeleton skeleton = CurrentUserTracker.CurrentSkeleton;
+
+        //Vector3 floorBasePoint = NuitrackManager.UserFrame.Floor.ToVector3() * 0.001f;
+        //Vector3 floorNormalVector = NuitrackManager.UserFrame.FloorNormal.ToVector3() * 0.0001f;
+
+        Vector3 floorBasePoint = floorBasePointTransform.position;
+        Vector3 floorNormalVector = (floorBasePointTransform.position - floorNormalVectorTransform.position).normalized;
+
+        //if (floorBasePoint != default && floorNormalVector != default)
+        floorPlane = new Plane(floorNormalVector, floorBasePoint);
+
+        //if (skeleton == null)
+        //{
+        //    CurrentMaxJumpHeigth = 0;
+        //    return;
+        //}
+
+        float bestJumpHipsY = ProjectHipY(skeleton, TotalMaxJumpHeigth);
+
+        bestJumpLine.anchoredPosition = new Vector2(0, bestJumpHipsY * baseRect.rect.height);
+        bestJumpLable.text = string.Format("Best: {0:F2}", TotalMaxJumpHeigth);
+
+        float currentJumpHipsY = ProjectHipY(skeleton);
+
+        currentJumpLine.anchoredPosition = new Vector2(0, currentJumpHipsY * baseRect.rect.height);
+        currentJumpLable.text = string.Format("Current: {0:F2}", CurrentMaxJumpHeigth);
+
+
+        float legLength = (LegLength(skeleton, leftLegJoints) + LegLength(skeleton, rightLegJoints)) / 2;
+
+        if (!LegOnFloor(skeleton))
+        {
+            Vector3 leftHipPosition = JointPoisition(skeleton, nuitrack.JointType.LeftHip);
+            Vector3 rightHipPosition = JointPoisition(skeleton, nuitrack.JointType.RightHip);
+
+            Vector3 floorLeftHipPoint = floorPlane.ClosestPointOnPlane(leftHipPosition);
+            Vector3 floorRightHipPoint = floorPlane.ClosestPointOnPlane(rightHipPosition);
+
+            float hipFloorDistance = (Vector3.Distance(leftHipPosition, floorLeftHipPoint) + Vector3.Distance(rightHipPosition, floorRightHipPoint)) / 2;
+            float deltaJump = hipFloorDistance - legLength;
+
+            Debug.Log(deltaJump);
+
+            if (deltaJump > CurrentMaxJumpHeigth)
+                CurrentMaxJumpHeigth = deltaJump;
+
+            if (CurrentMaxJumpHeigth > TotalMaxJumpHeigth)
+            {
+                TotalMaxJumpHeigth = CurrentMaxJumpHeigth;
+                bestJumpLine.gameObject.SetActive(true);
+            }
+        }
+
+    }
+   
+
+    void OnDrawGizmos()
+    {
+        Vector3 floorBasePoint = floorBasePointTransform.position;
+        Vector3 floorNormalVector = (floorBasePointTransform.position - floorNormalVectorTransform.position).normalized;
+
+        floorPlane = new Plane(floorNormalVector, floorBasePoint);
+
+        UnityEditor.Handles.color = new Color(0.5f, 1, 0.5f, 1f);
+        UnityEditor.Handles.DrawWireDisc(floorBasePoint, floorNormalVector, 2);
+
+        UnityEditor.Handles.DrawLine(floorBasePointTransform.position, floorNormalVectorTransform.position);
+
+        Vector3 leftAnkle = JointPoisition(null, nuitrack.JointType.LeftAnkle);
+        Vector3 rightAnkle = JointPoisition(null, nuitrack.JointType.RightAnkle);
+
+        Vector3 floorLeftAnklePoint = floorPlane.ClosestPointOnPlane(leftAnkle);
+        Vector3 floorRightAnklePoint = floorPlane.ClosestPointOnPlane(rightAnkle);
+
+        Gizmos.color = LegOnFloor(null) ? Color.green : Color.red;
+        Gizmos.DrawSphere(floorLeftAnklePoint, 0.1f);
+        Gizmos.DrawSphere(floorRightAnklePoint, 0.1f);
+    }
+}
---
+++ Assets/SkeletonEmulator/SkeletonEmulator.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+using System.Linq;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class JointSimulation
+{
+	public Transform targetTransform;
+	public nuitrack.JointType jointType;
+}
+
+public class SkeletonEmulator : MonoBehaviour
+{
+	[SerializeField] Transform spaceAxis;
+	[SerializeField] List<JointSimulation> jointSimulations;
+
+	Dictionary<nuitrack.JointType, Transform> jointsDictonary;
+
+    void Awake()
+    {
+		jointsDictonary = jointSimulations.ToDictionary(p => p.jointType, v => v.targetTransform);
+	}
+
+    public Vector3 JointPoisition(nuitrack.JointType jointType)
+	{
+		if (jointsDictonary != null)
+		{
+			if (jointsDictonary.ContainsKey(jointType))
+				return spaceAxis.InverseTransformPoint(jointsDictonary[jointType].position);
+			else
+			{
+				Debug.LogError(string.Format("Unknown joint: {0}", jointType.ToString()));
+				return Vector3.zero;
+			}
+		}
+		else
+        {
+			foreach (JointSimulation js in jointSimulations)
+				if (js.jointType == jointType)
+					return spaceAxis.InverseTransformPoint(js.targetTransform.position);
+
+			Debug.LogError(string.Format("Unknown joint: {0}", jointType.ToString()));
+			return Vector3.zero;
+		}
+	}
+
+    private void OnDrawGizmos()
+    {
+		Dictionary<nuitrack.JointType, Transform> skeleton = jointSimulations.ToDictionary(p => p.jointType, v => v.targetTransform);
+
+		UnityEditor.Handles.color = new Color(0.5f, 0.5f, 1, 0.3f);
+		foreach (Transform t in skeleton.Values)
+			UnityEditor.Handles.DrawSphere(0, t.position, Quaternion.identity, 0.05f);
+
+		UnityEditor.Handles.color = Color.white;
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.Waist].position, skeleton[nuitrack.JointType.Torso].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.Torso].position, skeleton[nuitrack.JointType.LeftCollar].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.Torso].position, skeleton[nuitrack.JointType.RightCollar].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.LeftCollar].position, skeleton[nuitrack.JointType.LeftShoulder].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.RightCollar].position, skeleton[nuitrack.JointType.RightShoulder].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.LeftCollar].position, skeleton[nuitrack.JointType.Head].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.RightCollar].position, skeleton[nuitrack.JointType.Head].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.Waist].position, skeleton[nuitrack.JointType.LeftHip].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.Waist].position, skeleton[nuitrack.JointType.RightHip].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.LeftHip].position, skeleton[nuitrack.JointType.LeftKnee].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.RightHip].position, skeleton[nuitrack.JointType.RightKnee].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.LeftKnee].position, skeleton[nuitrack.JointType.LeftAnkle].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.RightKnee].position, skeleton[nuitrack.JointType.RightAnkle].position);
+
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.LeftShoulder].position, skeleton[nuitrack.JointType.LeftElbow].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.RightShoulder].position, skeleton[nuitrack.JointType.RightElbow].position);
+
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.LeftElbow].position, skeleton[nuitrack.JointType.LeftWrist].position);
+		UnityEditor.Handles.DrawLine(skeleton[nuitrack.JointType.RightElbow].position, skeleton[nuitrack.JointType.RightWrist].position);
+	}
+}
