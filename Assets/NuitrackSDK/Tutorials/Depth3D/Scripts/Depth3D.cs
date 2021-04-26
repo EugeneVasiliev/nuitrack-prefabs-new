39f097133845b58e7927a30dc4276f601be43607
Evgeny <e.vasilev@3divi.com>
2021-04-26 14:06:12 +0500

    Add unlit shader, update skeleton controller & minor refactoring

---
+++ Assets/NuitrackSDK/Tutorials/Depth3D/Scripts/Depth3D.cs
@@ -0,0 +1,238 @@
+﻿using System.Runtime.InteropServices;
+
+using UnityEngine;
+
+public class Depth3D : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+
+    [Header ("RGB shader")]
+    Texture2D dstRgbTexture2D;
+
+    [Header("Segment shader")]
+    [SerializeField] ComputeShader depthToTexture;
+
+    [Range(-1.0f, 1.0f)]
+    [SerializeField] float contrast = 0f;
+
+    ComputeBuffer sourceDataBuffer;
+    byte[] depthDataArray = null;
+    RenderTexture depthRenderTexture;
+
+    uint xDepth, yDepth, zDepth;
+    int depthKernelIndex;
+
+    [Header ("Mesh generator")]
+    [SerializeField] MeshGenerator meshGenerator;
+
+    [Header ("Floor")]
+    Plane floorPlane;
+    [SerializeField] Transform rootMainScene;
+    [SerializeField] Transform floorRoot;
+
+    [SerializeField] float deltaHieght = 0.1f;
+    [SerializeField] float deltaAndle = 5f;
+    [SerializeField] float lerpMove = 4f;
+
+    float gravityMagnitude;
+
+    void Start()
+    {
+        depthKernelIndex = depthToTexture.FindKernel("Depth2Texture");
+        depthToTexture.GetKernelThreadGroupSizes(depthKernelIndex, out xDepth, out yDepth, out zDepth);
+
+        gravityMagnitude = Physics.gravity.magnitude;
+    }
+
+    Vector3 floorPoint;
+    Vector3 floorNormal;
+
+    void Update()
+    {
+        UpdateRGB();
+        FitMeshIntoFrame();
+
+        UpdateHieghtMap();
+        UpdateFloor();
+    }
+
+    void FitMeshIntoFrame()
+    {
+        nuitrack.ColorFrame frame = NuitrackManager.ColorFrame;
+
+        if (frame == null)
+            return;
+
+        float cameraToPlaneDist = Vector3.Distance(meshGenerator.transform.position, mainCamera.transform.position);
+        float frameAspectRatio = (float)frame.Cols / frame.Rows;
+
+        float v_angle = mainCamera.fieldOfView * Mathf.Deg2Rad * 0.5f;
+
+        float scale;
+
+        if (mainCamera.aspect < frameAspectRatio)
+        {
+            float radHFOV = Mathf.Atan(Mathf.Tan(v_angle) * mainCamera.aspect);
+            scale = cameraToPlaneDist * Mathf.Tan(radHFOV);
+        }
+        else
+        {
+            scale = cameraToPlaneDist * Mathf.Tan(v_angle);
+            scale *= (float)frame.Cols / frame.Rows;
+        }
+
+        meshGenerator.transform.localScale = new Vector3(scale * 2, scale * 2, 1);
+    }
+
+    void UpdateRGB()
+    {
+        nuitrack.ColorFrame frame = NuitrackManager.ColorFrame;
+
+        if (frame == null)
+            return;
+
+        if (dstRgbTexture2D == null)
+        {     
+            dstRgbTexture2D = new Texture2D(frame.Cols, frame.Rows, TextureFormat.RGB24, false);
+            meshGenerator.Generate(frame.Cols, frame.Rows);
+        }
+
+        dstRgbTexture2D.LoadRawTextureData(frame.Data, frame.DataSize);
+        dstRgbTexture2D.Apply();
+
+        meshGenerator.Material.SetTexture("_MainTex", dstRgbTexture2D);
+    }
+
+    void UpdateHieghtMap()
+    {
+        nuitrack.DepthFrame frame = NuitrackManager.DepthFrame;
+
+        if (frame == null)
+            return;
+
+        if (depthRenderTexture == null || depthRenderTexture.width != frame.Cols || depthRenderTexture.height != frame.Rows)
+        {
+            depthRenderTexture = new RenderTexture(frame.Cols, frame.Rows, 0, RenderTextureFormat.ARGB32);
+            depthRenderTexture.enableRandomWrite = true;
+            depthRenderTexture.filterMode = FilterMode.Point;
+            
+            depthRenderTexture.Create();
+
+            depthToTexture.SetInt("textureWidth", depthRenderTexture.width);
+            depthToTexture.SetTexture(depthKernelIndex, "Result", depthRenderTexture);
+            depthToTexture.SetFloat("contrast", 0);
+           
+            /*
+            We put the source data in the buffer, but the buffer does not support types 
+            that take up less than 4 bytes(instead of ushot(Int16), we specify uint(Int32)).
+
+            For optimization, we specify a length half the original length,
+            since the data is correctly projected into memory
+            (sizeof(ushot) * sourceDataBuffer / 2 == sizeof(uint) * sourceDataBuffer / 2)
+            */
+
+            int dataSize = frame.DataSize;
+            sourceDataBuffer = new ComputeBuffer(dataSize / 2, sizeof(uint));
+            depthToTexture.SetBuffer(depthKernelIndex, "DepthFrame", sourceDataBuffer);
+
+            depthDataArray = new byte[dataSize];
+        }
+
+        Marshal.Copy(frame.Data, depthDataArray, 0, depthDataArray.Length);
+        sourceDataBuffer.SetData(depthDataArray);
+     
+        depthToTexture.Dispatch(depthKernelIndex, depthRenderTexture.width / (int)xDepth, depthRenderTexture.height / (int)yDepth, (int)zDepth);
+
+        meshGenerator.Material.SetTexture("_HeightMap", depthRenderTexture);
+
+        Vector3 localCameraPosition = meshGenerator.transform.InverseTransformPoint(mainCamera.transform.position);
+        meshGenerator.Material.SetVector("_CameraPosition", localCameraPosition); 
+    }
+
+    void UpdateFloor()
+    {
+        nuitrack.UserFrame userFrame = NuitrackManager.UserFrame;
+
+        if (userFrame == null)
+            return;
+
+        Vector3 newFloorPoint = userFrame.Floor.ToVector3() * 0.001f;
+        Vector3 newFloorNormal = userFrame.FloorNormal.ToVector3().normalized;
+        Plane newFloor = new Plane(newFloorNormal, newFloorPoint);
+
+        if (floorPlane.Equals(default))
+        {
+            floorPoint = newFloorPoint;
+            floorNormal = newFloorNormal;
+            floorPlane = new Plane(floorNormal, floorPoint);
+        }
+
+        Vector3 newFloorSensor = newFloor.ClosestPointOnPlane(rootMainScene.position);
+        Vector3 floorSensor = floorPlane.ClosestPointOnPlane(rootMainScene.position);
+
+        if (Vector3.Angle(newFloor.normal, floorPlane.normal) >= deltaAndle || Mathf.Abs(newFloorSensor.y - floorSensor.y) >= deltaHieght)
+        {
+            floorPoint = newFloorPoint;
+            floorNormal = newFloorNormal;
+            floorPlane = new Plane(floorNormal, floorPoint);
+
+        }
+
+        Vector3 forward = rootMainScene.forward;
+        Vector3.OrthoNormalize(ref floorNormal, ref forward);
+
+        rootMainScene.localRotation = Quaternion.RotateTowards(rootMainScene.localRotation, Quaternion.LookRotation(forward, floorNormal), Time.deltaTime * lerpMove);
+
+        Vector3 localRoot = floorRoot.localPosition;
+        localRoot.y = floorSensor.y;
+        floorRoot.localPosition = Vector3.MoveTowards(floorRoot.localPosition, localRoot, Time.deltaTime * lerpMove);
+
+        Physics.gravity = -floorNormal * gravityMagnitude;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(depthRenderTexture);
+        Destroy(dstRgbTexture2D);
+    }
+
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.DrawWireDisc(floorPoint, floorNormal, 2f);
+
+        //float v_angle = mainCamera.fieldOfView * Mathf.Deg2Rad * 0.5f;
+        //float radHFOV = Mathf.Atan(Mathf.Tan(v_angle) * mainCamera.aspect);
+
+        ////nuitrack.ColorFrame frame = NuitrackManager.ColorFrame;
+
+        //float cameraToPlaneDist = Vector3.Distance(meshGenerator.transform.transform.position, mainCamera.transform.position);
+        //float frameAspectRatio = (float)frame.Cols / frame.Rows;
+
+        ////Debug.Log(string.Format("v_angle = {0}, h_angle = {1}, mainCamera.aspect = {2}", mainCamera.fieldOfView, radHFOV * Mathf.Rad2Deg, mainCamera.aspect));
+
+        //float scale;
+
+        //if (mainCamera.aspect < frameAspectRatio)
+        //    scale = cameraToPlaneDist * Mathf.Tan(radHFOV);
+        //else
+        //    scale = cameraToPlaneDist * Mathf.Tan(v_angle);
+
+        //Vector3[] polyLines = new Vector3[]
+        //{
+        //    new Vector3(-cameraToPlaneDist * Mathf.Tan(radHFOV), cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist),
+        //    new Vector3(cameraToPlaneDist * Mathf.Tan(radHFOV), cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist),
+        //    new Vector3(cameraToPlaneDist * Mathf.Tan(radHFOV), -cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist),
+        //    new Vector3(-cameraToPlaneDist * Mathf.Tan(radHFOV), -cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist),
+        //    new Vector3(-cameraToPlaneDist * Mathf.Tan(radHFOV), cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist),
+        // };
+
+        //UnityEditor.Handles.DrawPolyLine(polyLines);
+
+        ////Gizmos.DrawCube(new Vector3(0, 0, cameraToPlaneDist), Vector3.one * cameraToPlaneDist * Mathf.Tan(radHFOV));
+
+        //Gizmos.DrawCube(new Vector3(cameraToPlaneDist * Mathf.Tan(radHFOV), 0, cameraToPlaneDist), Vector3.one);
+        //Gizmos.DrawCube(new Vector3(0, cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist), Vector3.one);
+        //Gizmos.DrawCube(new Vector3(-cameraToPlaneDist * Mathf.Tan(radHFOV), 0, cameraToPlaneDist), Vector3.one);
+        //Gizmos.DrawCube(new Vector3(0, -cameraToPlaneDist * Mathf.Tan(v_angle), cameraToPlaneDist), Vector3.one);
+    }
+}
---
+++ Assets/NuitrackSDK/Tutorials/Depth3D/Scripts/RigidbodySkeletonController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RigidbodySkeletonController : MonoBehaviour
+{
+    [System.Serializable]
+    public class RigidbodyJoint
+    {
+        public Rigidbody rigidbody;
+        public nuitrack.JointType jointType;
+    }
+
+    [SerializeField] Transform space;
+    [SerializeField] List<RigidbodyJoint> rigidbodyJoints;
+
+    [SerializeField] Transform sensorCenter;
+    [SerializeField] GameObject jointObj;
+
+    [SerializeField] float smoothVal = 4f;
+
+    Dictionary<nuitrack.JointType, Transform> jointsObj;
+
+    void Start()
+    {
+        jointsObj = new Dictionary<nuitrack.JointType, Transform>();
+
+        foreach (nuitrack.JointType jointType in System.Enum.GetValues(typeof(nuitrack.JointType)))
+        {
+            GameObject joint = Instantiate(jointObj, sensorCenter);
+            joint.name = jointType.ToString();
+            jointsObj.Add(jointType, joint.transform);
+        }
+    }
+
+    void Update()
+    {
+        nuitrack.Skeleton skeleton = CurrentUserTracker.CurrentSkeleton;
+
+        if (skeleton == null)
+            return;
+
+        foreach (KeyValuePair<nuitrack.JointType, Transform> joints in jointsObj)
+        {
+            Vector3 position = skeleton.GetJoint(joints.Key).Real.ToVector3() * 0.001f;
+            joints.Value.localPosition = position;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        nuitrack.Skeleton skeleton = CurrentUserTracker.CurrentSkeleton;
+
+        if (skeleton == null)
+            return;
+
+        foreach(RigidbodyJoint rigidbodyJoint in rigidbodyJoints)
+        {
+            Vector3 newPosition = skeleton.GetJoint(rigidbodyJoint.jointType).Real.ToVector3() * 0.001f;
+            Vector3 spacePostion = space.TransformPoint(newPosition);
+
+            Vector3 lerpPosition = Vector3.Lerp(rigidbodyJoint.rigidbody.position, spacePostion, Time.deltaTime * smoothVal);
+
+            rigidbodyJoint.rigidbody.MovePosition(lerpPosition);
+        }
+    }
+}
