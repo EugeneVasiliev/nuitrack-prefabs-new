4cbb0b905626a888ad9a69bddcec9647f2194c4f
Alexander Verevkin <a.verevkin@3divi.com>
2021-05-13 09:32:32 +0500

    Changed the source branch

--- Scripts/requirements.bat
+++ Scripts/requirements.bat
@@ -3,12 +3,14 @@ cd C:\Users\stranger\AppData\Local\Programs\Python\Python*
 set PYTHON_PATH=%cd%
 popd
 
+set DEPTH_TAG=Fittar_develop
+
 %PYTHON_PATH%\Scripts\pip.exe install git+http://gitlab-ci-token:%CI_JOB_TOKEN%@tox.3divi.ru/3divi-devops/3divi-ctl-client.git@windows_fix
 
-%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/arm32 artifact://3d/depth_scanner/ubuntu-14.04.5-android-arm32:Fittar_v0.35.1_v2.1
-%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/arm64 artifact://3d/depth_scanner/ubuntu-14.04.5-android-arm64:Fittar_v0.35.1_v2.1
-%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/lin64 artifact://3d/depth_scanner/ai_ubuntu-14.04.5-linux-x64:Fittar_v0.35.1_v2.1
-%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/ios64 artifact://3d/depth_scanner/macos-ios-arm64:Fittar_v0.35.1_v2.1
+%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/arm32 artifact://3d/depth_scanner/ubuntu-14.04.5-android-arm32:%DEPTH_TAG%
+%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/arm64 artifact://3d/depth_scanner/ubuntu-14.04.5-android-arm64:%DEPTH_TAG%
+%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/lin64 artifact://3d/depth_scanner/ai_ubuntu-14.04.5-linux-x64:%DEPTH_TAG%
+%PYTHON_PATH%\python.exe %PYTHON_PATH%\Scripts\3divi-ctl get --output ../depth_scanner/ios64 artifact://3d/depth_scanner/macos-ios-arm64:%DEPTH_TAG%
 
 copy /y "..\depth_scanner\arm64\depth_scanner\deployment\NuitrackSDK\Nuitrack\lib\android-arm64\*.jar"     "Assets/NuitrackSDK/Nuitrack/NuitrackAssembly"
 
