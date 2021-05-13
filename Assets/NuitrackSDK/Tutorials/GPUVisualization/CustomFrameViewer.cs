afd40c9929e0ded4d620640ada45d5dfd481d4c7
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-05-13 17:24:52 +0500

    Add custaom frame viewer

---
+++ Assets/NuitrackSDK/Tutorials/GPUVisualization/CustomFrameViewer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class CustomFrameViewer : MonoBehaviour
+{
+    public enum Mode
+    {
+        Cut,
+        ReverseCut,
+        Mul,
+        Mix
+    }
+
+    [SerializeField] Texture mainTexture;
+    [SerializeField] Texture altTexture;
+
+    [SerializeField] Mode mode = Mode.Cut;
+    [SerializeField] FrameViewer.TextureEvent onFrameUpdate;
+
+    RenderTexture renderTexture;
+
+    void Update()
+    {
+        switch(mode)
+        {
+            case Mode.Cut:
+                FrameProvider.FrameUtils.Cut(mainTexture, altTexture, ref renderTexture);
+                break;
+            case Mode.ReverseCut:
+                FrameProvider.FrameUtils.ReverseCut(mainTexture, altTexture, ref renderTexture);
+                break;
+            case Mode.Mul:
+                FrameProvider.FrameUtils.Mul(mainTexture, altTexture, ref renderTexture);
+                break;
+            case Mode.Mix:
+                FrameProvider.FrameUtils.MixMask(mainTexture, altTexture, ref renderTexture);
+                break;
+        }
+
+        onFrameUpdate.Invoke(renderTexture);
+    }
+
+    public void MainTexture(Texture texture)
+    {
+        mainTexture = texture;
+    }
+
+    public void AltTexture(Texture texture)
+    {
+        altTexture = texture;
+    }
+}
