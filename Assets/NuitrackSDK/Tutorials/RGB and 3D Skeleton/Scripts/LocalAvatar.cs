564b3ea4a9f7d23a16707b5392cfdbaeac64542c
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-05-14 18:49:48 +0500

    Add RGB and 3D Skeleton Example (DRAFT)

---
+++ Assets/NuitrackSDK/Tutorials/RGB and 3D Skeleton/Scripts/LocalAvatar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LocalAvatar : MonoBehaviour
+{
+    [Header("Main")]
+    [SerializeField, Range(1, 6)] int skeletonID = 1;
+    [SerializeField] Transform space;
+
+    [Header("Rigged model")]
+    [SerializeField] List<ModelJoint> modelJoints;
+
+    Dictionary<nuitrack.JointType, ModelJoint> jointsRigged = new Dictionary<nuitrack.JointType, ModelJoint>();
+
+    ulong lastTimeStamp = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < modelJoints.Count; i++)
+        {
+            modelJoints[i].baseRotOffset = Quaternion.Inverse(space.rotation) * modelJoints[i].bone.rotation;
+            jointsRigged.Add(modelJoints[i].jointType.TryGetMirrored(), modelJoints[i]);
+        }
+    }
+
+    void Update()
+    {
+        if (NuitrackManager.SkeletonData == null || NuitrackManager.SkeletonData.Timestamp == lastTimeStamp)
+            return;
+
+        lastTimeStamp = NuitrackManager.SkeletonData.Timestamp;
+
+        nuitrack.Skeleton skeleton = NuitrackManager.SkeletonData.GetSkeletonByID(skeletonID);
+
+        if (skeleton == null)
+            return;
+
+        foreach (var riggedJoint in jointsRigged)
+        {
+            nuitrack.Joint joint = skeleton.GetJoint(riggedJoint.Key);
+            ModelJoint modelJoint = riggedJoint.Value;
+
+            //Bone position
+            Vector3 newPos = joint.ToVector3() * 0.001f;
+            modelJoint.bone.position = space.TransformPoint(newPos);
+
+            //Bone rotation
+            Quaternion jointOrient = Quaternion.Inverse(CalibrationInfo.SensorOrientation) * joint.ToQuaternion() * modelJoint.baseRotOffset;
+            modelJoint.bone.rotation = space.rotation * jointOrient;
+        }
+    }
+}
