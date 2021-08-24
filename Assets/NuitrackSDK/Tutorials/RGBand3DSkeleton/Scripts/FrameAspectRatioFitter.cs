cd15ffbb73aca8ee1d87979a998eab58e35b6f08
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-08-24 15:41:27 +0500

    Edit set camera fov for sensor fov

--- Assets/NuitrackSDK/Tutorials/RGBand3DSkeleton/Scripts/FrameAspectRatioFitter.cs
+++ Assets/NuitrackSDK/Tutorials/RGBand3DSkeleton/Scripts/FrameAspectRatioFitter.cs
@@ -6,7 +6,10 @@ namespace NuitrackSDK.Tutorials.RGBand3DSkeleton
     public class FrameAspectRatioFitter : MonoBehaviour
     {
         [SerializeField] new Camera camera;
+
+        [SerializeField] RectTransform screenCanvas;
         [SerializeField] AspectRatioFitter aspectRatioFitter;
+
 
         bool waitEvent = true;
 
@@ -33,11 +36,20 @@ namespace NuitrackSDK.Tutorials.RGBand3DSkeleton
 
             nuitrack.OutputMode mode = NuitrackManager.DepthSensor.GetOutputMode();
 
-            // Setting the camera's vFOV equal to the depth sensor's vFOV. 
-            // Nuitrack does not yet have a representation of vFOV, so we use the hFOV to vFOV conversion.
-            float vFOV = 2 * Mathf.Atan(Mathf.Tan(mode.HFOV * 0.5f) * ((float)frame.Rows / frame.Cols));
+            float sensorFrameAspectRatio = (float)frame.Cols / frame.Rows;
+            float screenAspectRatio = (float)screenCanvas.rect.width / screenCanvas.rect.height;
 
-            camera.fieldOfView = vFOV * Mathf.Rad2Deg;
+            if(screenAspectRatio > sensorFrameAspectRatio)
+            {
+                // Setting the camera's vFOV equal to the depth sensor's vFOV. 
+                // Nuitrack does not yet have a representation of vFOV, so we use the hFOV to vFOV conversion.
+                float vFOV = 2 * Mathf.Atan(Mathf.Tan(mode.HFOV * 0.5f) * ((float)frame.Rows / frame.Cols));
+                camera.fieldOfView = vFOV * Mathf.Rad2Deg;
+            }
+            else
+            {
+
+            } 
         }
     }
 }
