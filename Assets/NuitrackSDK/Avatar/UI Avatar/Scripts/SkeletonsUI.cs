0a2ee6c2ff072c2e271985e288606406444fcd59
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-09-30 18:01:35 +0500

    Switch UIAvatar to BaseAvatar

--- Assets/NuitrackSDK/Avatar/UI Avatar/Scripts/SkeletonsUI.cs
+++ Assets/NuitrackSDK/Avatar/UI Avatar/Scripts/SkeletonsUI.cs
@@ -2,48 +2,41 @@
 using UnityEngine;
 using nuitrack;
 
-public class SkeletonsUI : MonoBehaviour
+namespace NuitrackSDK.Avatar
 {
-    [SerializeField] RectTransform spawnRectTransform;
-
-    [SerializeField, Range(0, 6)] int skeletonCount = 6;         //Max number of skeletons tracked by Nuitrack
-    [SerializeField] UIAvatar skeletonAvatar;
-
-    List<UIAvatar> avatars = new List<UIAvatar>();
-
-    void Start()
+    public class SkeletonsUI : MonoBehaviour
     {
-        for (int i = 0; i < skeletonCount; i++)
+        [SerializeField] RectTransform spawnRectTransform;
+
+        [SerializeField, Range(0, 6)] int skeletonCount = 6;         //Max number of skeletons tracked by Nuitrack
+        [SerializeField] UIAvatar skeletonAvatar;
+
+        List<UIAvatar> avatars = new List<UIAvatar>();
+
+        void Start()
         {
-            GameObject newAvatar = Instantiate(skeletonAvatar.gameObject, spawnRectTransform);
-            UIAvatar skeleton = newAvatar.GetComponent<UIAvatar>();
-            skeleton.autoProcessing = false;
-            avatars.Add(skeleton);
+            for (int i = 0; i < skeletonCount; i++)
+            {
+                GameObject newAvatar = Instantiate(skeletonAvatar.gameObject, spawnRectTransform);
+                UIAvatar skeleton = newAvatar.GetComponent<UIAvatar>();
+                skeleton.SkeletonID = i + 1;
+                avatars.Add(skeleton);
+            }
+
+            NuitrackManager.SkeletonTracker.SetNumActiveUsers(skeletonCount);
+
+            NuitrackManager.onSkeletonTrackerUpdate += OnSkeletonUpdate;
         }
 
-        NuitrackManager.SkeletonTracker.SetNumActiveUsers(skeletonCount);
-
-        NuitrackManager.onSkeletonTrackerUpdate += OnSkeletonUpdate;
-    }
-
-    void OnSkeletonUpdate(SkeletonData skeletonData)
-    {
-        for (int i = 0; i < avatars.Count; i++)
+        void OnSkeletonUpdate(SkeletonData skeletonData)
         {
-            if (i < skeletonData.Skeletons.Length)
-            {
-                avatars[i].gameObject.SetActive(true);
-                avatars[i].ProcessSkeleton(skeletonData.Skeletons[i]);
-            }
-            else
-            {
-                avatars[i].gameObject.SetActive(false);
-            }
+            for (int i = 0; i < avatars.Count; i++)
+                avatars[i].gameObject.SetActive(i < skeletonData.Skeletons.Length);
         }
-    }
 
-    private void OnDestroy()
-    {
-        NuitrackManager.onSkeletonTrackerUpdate -= OnSkeletonUpdate;
+        private void OnDestroy()
+        {
+            NuitrackManager.onSkeletonTrackerUpdate -= OnSkeletonUpdate;
+        }
     }
 }
