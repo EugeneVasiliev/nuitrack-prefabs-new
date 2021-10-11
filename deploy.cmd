f141a5a464337b5ad59422764de4eb79a1651214
Stepan Reuk <stepan.reuk@3divi.com>
2021-10-11 18:03:40 +0500

    added echo off

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,5 @@
+@echo off
 echo start
-rem "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -manualLicenseFile Unity_v2020.x.ulf
-rem "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildApk NuitrackDemo.apk -logFile log.txt
 "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -exportPackage "Assets/NuitrackSDK" "Assets/Plugins" NuitrackSDK.unitypackage
 "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildAab NuitrackDemo.aab
 "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildApk NuitrackDemo.apk
