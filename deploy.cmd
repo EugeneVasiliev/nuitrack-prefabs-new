d26d6b04adf2ba0ee6b47af6ce2b2485ec532682
Stepan Reuk <stepan.reuk@3divi.com>
2021-10-11 17:09:17 +0500

    upgate gitlab-ci.yml

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 echo start
 "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -manualLicenseFile Unity_v2020.x.ulf
-"C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildApk NuitrackDemo.apk -logFile log.txt
-rem "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath . -exportPackage "Assets/NuitrackSDK" "Assets/Plugins" NuitrackSDK.unitypackage
+rem "C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildApk NuitrackDemo.apk -logFile log.txt
+"C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -exportPackage "Assets/NuitrackSDK" "Assets/Plugins" NuitrackSDK.unitypackage -logFile log.txt
 echo finish
