dc2e3691502b7dcca8888ed40e326e041c0322ea
Stepan Reuk <stepan.reuk@3divi.com>
2021-10-11 11:48:57 +0500

    simplified build pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-"C:\Program Files\Unity Editors\Unity2020.3.1\Editor\Unity.exe" -batchmode -quit -projectPath . -executeMethod BuildMyGame.BuildAab NuitrackDemo.aab
-"C:\Program Files\Unity Editors\Unity2020.3.1\Editor\Unity.exe" -batchmode -quit -projectPath . -executeMethod BuildMyGame.BuildApk NuitrackDemo.apk
+echo start
 "C:\Program Files\Unity Editors\Unity2020.3.1\Editor\Unity.exe" -batchmode -quit -projectPath . -exportPackage "Assets/NuitrackSDK" "Assets/Plugins" NuitrackSDK.unitypackage
-mkdir build
-"C:\Program Files\Unity Editors\Unity2020.3.1\Editor\Unity.exe" -batchmode -quit -projectPath . -executeMethod BuildMyGame.BuildLinux "build\Nui_Test.x86_64"
-powershell Compress-Archive build linux_build.zip
+echo finish
