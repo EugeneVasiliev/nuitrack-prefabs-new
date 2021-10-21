279009caa56e4efdc110b4b54a6ef394f8e2aab6
Evgeny Vasilev <e.vasilev@3divi.com>
2021-10-21 09:08:36 +0000

    NuitrackSDK tutorials asset

--- Assets/NuitrackSDK/Nuitrack/Scripts/Editor/GUI/NuitrackSDKGUI.cs
+++ Assets/NuitrackSDK/Nuitrack/Scripts/Editor/GUI/NuitrackSDKGUI.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 
 using System;
 using System.IO;
-
+using UnityEngine.Events;
 
 namespace NuitrackSDKEditor
 {
@@ -11,6 +11,7 @@ namespace NuitrackSDKEditor
     /// Put the <see cref="GUI"/> block code in the using statement to color the <see cref="GUI"/> elements in the specified color
     /// After the using block, the <see cref="GUI"/> color will return to the previous one
     ///
+    /// <para>
     /// <example>
     /// This shows how to change the GUI color
     /// <code>
@@ -20,6 +21,7 @@ namespace NuitrackSDKEditor
     /// }
     /// </code>
     /// </example>
+    /// </para>
     /// </summary>
     public class GUIColor : IDisposable
     {
@@ -41,6 +43,7 @@ namespace NuitrackSDKEditor
     /// Put the <see cref="Handles"/> block code in the using statement to color the <see cref="Handles"/> elements in the specified color
     /// After the using block, the <see cref="Handles"/> color will return to the previous one
     ///
+    /// <para>
     /// <example>
     /// This shows how to change the Handles color
     /// <code>
@@ -50,6 +53,7 @@ namespace NuitrackSDKEditor
     /// }
     /// </code>
     /// </example>
+    /// </para>
     /// </summary>
     public class HandlesColor : IDisposable
     {
@@ -68,28 +72,98 @@ namespace NuitrackSDKEditor
     }
 
     /// <summary>
+    /// Place GUI elements in a horizontal group <seealso cref="EditorGUILayout.BeginHorizontal(GUIStyle, GUILayoutOption[])"/>
+    /// 
+    /// <para>
+    /// <example>
+    /// This shows how to put GUI elements in a horizontal group via using
+    /// <code>
+    /// using (new HorizontalGroup())
+    /// {
+    ///     // Your GUI code ...
+    /// }
+    /// </code>
+    /// </example>
+    /// </para>
+    /// </summary>
+    public class HorizontalGroup : IDisposable
+    {
+        /// <summary>
+        /// Create a horizontal group
+        /// </summary>
+        /// <param name="guiStyle">GUI style (default is GUIStyle.none)</param>
+        /// <param name="options">GUI layout option (GUILayout.Width, GUILayout.MinWidth and others)</param>
+        public HorizontalGroup(GUIStyle guiStyle = null, GUILayoutOption[] options = null)
+        {
+            guiStyle ??= GUIStyle.none;
+            EditorGUILayout.BeginHorizontal(guiStyle, options);
+        }
+
+        public void Dispose()
+        {
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    /// <summary>
+    /// Place GUI elements in a vertical group <seealso cref="EditorGUILayout.BeginVertical(GUIStyle, GUILayoutOption[])"/>
+    /// 
+    /// <para>
+    /// <example>
+    /// This shows how to put GUI elements in a vertical group via using
+    /// <code>
+    /// using (new VerticalGroup())
+    /// {
+    ///     // Your GUI code ...
+    /// }
+    /// </code>
+    /// </example>
+    /// </para>
+    /// </summary>
+    public class VerticalGroup : IDisposable
+    {
+        /// <summary>
+        /// Create a vertical group
+        /// </summary>
+        /// <param name="guiStyle">GUI style (default is GUIStyle.none)</param>
+        /// <param name="options">GUI layout option (GUILayout.Width, GUILayout.MinWidth and others)</param>
+        public VerticalGroup(GUIStyle styles = null, GUILayoutOption[] options = null)
+        {
+            styles ??= GUIStyle.none;
+            EditorGUILayout.BeginVertical(styles, options);
+        }
+
+        public void Dispose()
+        {
+            EditorGUILayout.EndVertical();
+        }
+    }
+
+    /// <summary>
     /// GUI draw helper class
     /// </summary>
     public static class NuitrackSDKGUI
     {
         /// <summary>
-        /// Draw the "Help" button. Return a rectangle to draw your GUI element with an indent for the button.
+        ///  Draw an additional button to the right of the GUI element (for example, the clear or help button)
+        ///  Return a rectangle to draw your GUI element with an indent for the button.
         /// </summary>
-        /// <param name="url">Click-through link</param>
+        /// <param name="buttonAction">Action when clicking on an button</param>
+        /// <param name="iconName">Name of the icon for the button</param>
         /// <param name="tooltip">(optional) ToolTip displayed when hovering over the button</param>
         /// <returns>Rectangle to draw your GUI element with an indent for the button.</returns>
-        public static Rect WithHelpButton(string url, string tooltip = "")
+        public static Rect WithRightButton(UnityAction buttonAction, string iconName, string tooltip = "")
         {
-            GUIContent helpButton = EditorGUIUtility.IconContent("_Help");
-            helpButton.tooltip = tooltip;
+            GUIContent buttonContent = EditorGUIUtility.IconContent(iconName);
+            buttonContent.tooltip = tooltip;
 
             Rect main = EditorGUILayout.GetControlRect();
-            main.xMax -= helpButton.image.width;
+            main.xMax -= buttonContent.image.width;
 
-            Rect helpButtonRect = new Rect(main.x + main.width, main.y, helpButton.image.width, main.height);
+            Rect addButtonRect = new Rect(main.x + main.width, main.y, buttonContent.image.width, main.height);
 
-            if (GUI.Button(helpButtonRect, helpButton, GUIStyle.none))
-                Application.OpenURL(url);
+            if (GUI.Button(addButtonRect, buttonContent, GUIStyle.none))
+                buttonAction.Invoke();
 
             return main;
         }
@@ -105,7 +179,9 @@ namespace NuitrackSDKEditor
         {
             SerializedProperty property = serializedObject.FindProperty(propertyName);
 
-            Rect propertyRect = WithHelpButton(url, toolTip);
+            UnityAction helpClick = delegate { Application.OpenURL(url); };
+
+            Rect propertyRect = WithRightButton(helpClick, "_Help", toolTip);
 
             EditorGUI.PropertyField(propertyRect, property);
             serializedObject.ApplyModifiedProperties();
@@ -124,7 +200,7 @@ namespace NuitrackSDKEditor
             GUIContent browseButtonContent = EditorGUIUtility.IconContent("Project");
             browseButtonContent.text = "Browse";
 
-            GUIContent clearButtonContent = EditorGUIUtility.IconContent("d_TreeEditor.Trash");
+            GUIContent clearButtonContent = EditorGUIUtility.IconContent("TreeEditor.Trash");
             clearButtonContent.text = "Clear";
 
             GUIContent errorMessage = EditorGUIUtility.IconContent("console.erroricon.sml");
@@ -175,7 +251,10 @@ namespace NuitrackSDKEditor
             EditorGUI.BeginDisabledGroup(path == string.Empty);
 
             if (GUILayout.Button(clearButtonContent))
+            {
                 path = string.Empty;
+                GUIUtility.keyboardControl = 0;
+            }
 
             EditorGUI.EndDisabledGroup();
 
