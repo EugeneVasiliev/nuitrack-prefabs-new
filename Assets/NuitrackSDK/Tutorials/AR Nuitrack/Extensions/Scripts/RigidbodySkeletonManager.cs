d993cadb404105917d2dcc5b81e3fbe62ea2cdaf
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-10-28 13:49:30 +0500

    Update namespace for AR Nuitrack

--- Assets/NuitrackSDK/Tutorials/AR Nuitrack/Extensions/Scripts/RigidbodySkeletonManager.cs
+++ Assets/NuitrackSDK/Tutorials/AR Nuitrack/Extensions/Scripts/RigidbodySkeletonManager.cs
@@ -1,31 +1,35 @@
 ﻿using UnityEngine;
-
 using System.Collections.Generic;
 
-public class RigidbodySkeletonManager : MonoBehaviour
+
+namespace NuitrackSDK.Tutorials.ARNuitrack.Extensions
 {
-    [SerializeField] GameObject rigidBodySkeletonPrefab;
-    [SerializeField] Transform space;
-
-    Dictionary<int, RigidbodySkeletonController> skeletons = new Dictionary<int, RigidbodySkeletonController>();
-
-    void Update()
+    [AddComponentMenu("NuitrackSDK/Tutorials/AR Nuitrack/Extensions/Rigidbody Skeleton Manager")]
+    public class RigidbodySkeletonManager : MonoBehaviour
     {
-        foreach (UserData user in NuitrackManager.Users)
-            if (!skeletons.ContainsKey(user.ID))
-            {
-                RigidbodySkeletonController rigidbodySkeleton = Instantiate(rigidBodySkeletonPrefab, space).GetComponent<RigidbodySkeletonController>();
-                rigidbodySkeleton.UserID = user.ID;
-                rigidbodySkeleton.SetSpace(space);
+        [SerializeField] GameObject rigidBodySkeletonPrefab;
+        [SerializeField] Transform space;
 
-                skeletons.Add(user.ID, rigidbodySkeleton);
-            }
+        Dictionary<int, RigidbodySkeletonController> skeletons = new Dictionary<int, RigidbodySkeletonController>();
 
-        foreach (int skeletonID in new List<int>(skeletons.Keys))
-            if (NuitrackManager.Users.GetUser(skeletonID) == null)
-            {
-                Destroy(skeletons[skeletonID].gameObject);
-                skeletons.Remove(skeletonID);
-            }
+        void Update()
+        {
+            foreach (UserData user in NuitrackManager.Users)
+                if (!skeletons.ContainsKey(user.ID))
+                {
+                    RigidbodySkeletonController rigidbodySkeleton = Instantiate(rigidBodySkeletonPrefab, space).GetComponent<RigidbodySkeletonController>();
+                    rigidbodySkeleton.UserID = user.ID;
+                    rigidbodySkeleton.SetSpace(space);
+
+                    skeletons.Add(user.ID, rigidbodySkeleton);
+                }
+
+            foreach (int skeletonID in new List<int>(skeletons.Keys))
+                if (NuitrackManager.Users.GetUser(skeletonID) == null)
+                {
+                    Destroy(skeletons[skeletonID].gameObject);
+                    skeletons.Remove(skeletonID);
+                }
+        }
     }
 }
