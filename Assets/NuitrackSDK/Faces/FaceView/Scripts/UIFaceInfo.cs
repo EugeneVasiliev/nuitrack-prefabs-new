15160efffe9785e09f259bbbb5d9ce9404b414f7
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-10-28 13:47:21 +0500

    Update namespace for Faces

--- Assets/NuitrackSDK/Faces/FaceView/Scripts/UIFaceInfo.cs
+++ Assets/NuitrackSDK/Faces/FaceView/Scripts/UIFaceInfo.cs
@@ -2,77 +2,81 @@
 using UnityEngine.UI;
 
 
-public class UIFaceInfo : MonoBehaviour
+namespace NuitrackSDK.Face
 {
-    [Header("Info")]
-    public bool autoProcessing;
-    [SerializeField] bool showInfo = true;
-    [SerializeField] GameObject infoPanel;
-    [SerializeField] Text ageText;
-    [SerializeField] Text yearsText;
-    [SerializeField] Text genderText;
-    [SerializeField] Slider neutral;
-    [SerializeField] Slider angry;
-    [SerializeField] Slider surprise;
-    [SerializeField] Slider happy;
-
-    RectTransform spawnTransform;
-    RectTransform frameTransform;
-    Image image;
-
-    public void Initialize(RectTransform spawnTransform)
+    [AddComponentMenu("NuitrackSDK/Face/Face View/UI Face Info")]
+    public class UIFaceInfo : MonoBehaviour
     {
-        this.spawnTransform = spawnTransform;
+        [Header("Info")]
+        public bool autoProcessing;
+        [SerializeField] bool showInfo = true;
+        [SerializeField] GameObject infoPanel;
+        [SerializeField] Text ageText;
+        [SerializeField] Text yearsText;
+        [SerializeField] Text genderText;
+        [SerializeField] Slider neutral;
+        [SerializeField] Slider angry;
+        [SerializeField] Slider surprise;
+        [SerializeField] Slider happy;
 
-        frameTransform = GetComponent<RectTransform>();
-        image = frameTransform.GetComponent<Image>();
-    }
+        RectTransform spawnTransform;
+        RectTransform frameTransform;
+        Image image;
 
-    void Update()
-    {
-        if (autoProcessing)
+        public void Initialize(RectTransform spawnTransform)
         {
-            ProcessFace(NuitrackManager.Users.Current);
+            this.spawnTransform = spawnTransform;
+
+            frameTransform = GetComponent<RectTransform>();
+            image = frameTransform.GetComponent<Image>();
         }
-    }
 
-    public void ProcessFace(UserData userData)
-    {
-        if (!NuitrackManager.Instance.UseFaceTracking)
-            Debug.Log("Attention: Face tracking disabled! Enable it on the Nuitrack Manager component");
-
-        if (userData == null)
-            return;
-
-        Face currentFace = userData.Face;
-
-        if (currentFace != null && spawnTransform)
+        void Update()
         {
-            image.enabled = true;
-            infoPanel.SetActive(showInfo);
-
-            Rect faceRect = currentFace.Rect;
-
-            Vector2 newPosition = new Vector2(
-                spawnTransform.rect.width * (faceRect.x - 0.5f) + frameTransform.rect.width / 2,
-                spawnTransform.rect.height * (0.5f - faceRect.y) - frameTransform.rect.height / 2);
-
-            frameTransform.sizeDelta = new Vector2(faceRect.width * spawnTransform.rect.width, faceRect.height * spawnTransform.rect.height);
-            frameTransform.anchoredPosition = newPosition;
-
-            ageText.text = currentFace.AgeType.ToString();
-            yearsText.text = string.Format("Years: {0:F1}", currentFace.age.years);
-            genderText.text = currentFace.Gender.ToString();
-
-            neutral.value = currentFace.GetEmotionValue(Emotions.Type.neutral);
-            angry.value = currentFace.GetEmotionValue(Emotions.Type.angry);
-            surprise.value = currentFace.GetEmotionValue(Emotions.Type.surprise);
-            happy.value = currentFace.GetEmotionValue(Emotions.Type.happy);
+            if (autoProcessing)
+            {
+                ProcessFace(NuitrackManager.Users.Current);
+            }
         }
-        else
+
+        public void ProcessFace(UserData userData)
         {
-            image.enabled = false;
-            infoPanel.SetActive(false);
+            if (!NuitrackManager.Instance.UseFaceTracking)
+                Debug.Log("Attention: Face tracking disabled! Enable it on the Nuitrack Manager component");
+
+            if (userData == null)
+                return;
+
+            nuitrack.Face currentFace = userData.Face;
+
+            if (currentFace != null && spawnTransform)
+            {
+                image.enabled = true;
+                infoPanel.SetActive(showInfo);
+
+                Rect faceRect = currentFace.Rect;
+
+                Vector2 newPosition = new Vector2(
+                    spawnTransform.rect.width * (faceRect.x - 0.5f) + frameTransform.rect.width / 2,
+                    spawnTransform.rect.height * (0.5f - faceRect.y) - frameTransform.rect.height / 2);
+
+                frameTransform.sizeDelta = new Vector2(faceRect.width * spawnTransform.rect.width, faceRect.height * spawnTransform.rect.height);
+                frameTransform.anchoredPosition = newPosition;
+
+                ageText.text = currentFace.AgeType.ToString();
+                yearsText.text = string.Format("Years: {0:F1}", currentFace.age.years);
+                genderText.text = currentFace.Gender.ToString();
+
+                neutral.value = currentFace.GetEmotionValue(nuitrack.Emotions.Type.neutral);
+                angry.value = currentFace.GetEmotionValue(nuitrack.Emotions.Type.angry);
+                surprise.value = currentFace.GetEmotionValue(nuitrack.Emotions.Type.surprise);
+                happy.value = currentFace.GetEmotionValue(nuitrack.Emotions.Type.happy);
+            }
+            else
+            {
+                image.enabled = false;
+                infoPanel.SetActive(false);
+            }
         }
     }
 }
