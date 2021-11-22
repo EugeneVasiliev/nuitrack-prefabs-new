94efd0e1f7b8f37774fcbeec92627d4b1f0d8c11
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-11-22 16:25:10 +0500

    Prepare to github

--- deploy_apk.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo start
-"C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildApk NuitrackDemo.apk
-echo finish
--- deploy_linux.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo start
-mkdir build
-"C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -executeMethod BuildMyGame.BuildLinux "build\Nui_Test.x86_64"
-powershell Compress-Archive build linux_build.zip
-
-echo finish
