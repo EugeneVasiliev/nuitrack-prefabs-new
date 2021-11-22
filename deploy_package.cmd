bbe64077eb86c4c22767ec8b4a7389262c40ffc1
Evgeniy Vasilev <e.vasilev@3divi.com>
2021-11-22 16:43:50 +0500

    Update to package

--- deploy_package.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo start
-"C:\Editors\2020.3.1f1\Editor\Unity.exe" -batchmode -quit -projectPath %cd% -exportPackage "Assets/NuitrackSDK" "Assets/Plugins" NuitrackSDK.unitypackage
-echo finish
